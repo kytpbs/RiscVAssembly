1d3ece5c1dfc1ad843971928027d0e69562381cf
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-03-21 01:44:57 +0100

    make sure LLVM path is added in run script

--- run.ps1
+++ run.ps1
@@ -1,4 +1,5 @@
 $ErrorActionPreference = "Stop"
+$ENV:path += ";C:\llvm\bin" # Add LLVM to PATH, couldn't get PATH to work for some reason...
 
 clang --target=riscv64 -march=rv64gc -mabi=lp64d -mno-relax app.s -c -o build/app.o
 ld.lld build/app.o -o build/app.x

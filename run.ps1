19792041b8ae211a787172389025dcee0b084dcc
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-03-21 01:25:21 +0100

    update to 64 bit

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = "Stop"
 
-clang --target=riscv32 -march=rv32gc -mabi=ilp32d -mno-relax hello.s -c -o hello.o
+clang --target=riscv64 -march=rv64gc -mabi=lp64d -mno-relax hello.s -c -o hello.o
 ld.lld hello.o -o hello
-wsl qemu-riscv32 ./hello
+wsl qemu-riscv64 ./hello

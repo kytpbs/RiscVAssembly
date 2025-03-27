bddd613ff263bb075b3f5938d0a20926c46688aa
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-03-27 02:11:28 +0100

    [PROGRAM-6] countdown from a given value

--- app.s
+++ app.s
@@ -1,46 +1,28 @@
 .data
 	ask_value_1: 
-		.asciz "Enter the first value: "
-	ask_value_2: 
-		.asciz "Enter the second value: "
+		.asciz "Countdown: "
 
 .text
 .globl _start
 
 _start:
-_ask_first_value:
+_ask_countdown_value:
 	lui a0, %hi(ask_value_1)
 	addi a0, a0, %lo(ask_value_1)
 	li a7, 4 # print string -> prints a0 until null
 	ecall
 
-_read_first_value:
+_read_countdown_value:
 	li a7, 5 # ReadInt -> outputs to a0
 	ecall
-	mv t0, a0
 
-_ask_second_value:
-	lui a0, %hi(ask_value_2)
-	addi a0, a0, %lo(ask_value_2)
-	li a7, 4 # print string -> prints a0 until null
-	ecall
-
-_read_second_value:
-	li a7, 5 # ReadInt -> outputs to a0
-	ecall
-	mv t1, a0
-
-_find_bigger_value:
-	bge t1, t0, _smaller_value
-	mv a0, t0
-	j _print_a0_int 
-
-_smaller_value:
-	mv a0, t1
-
-_print_a0_int:
+_prepare_print:
 	li a7, 1 # PrintInt -> prints a0
-	ecall
+
+_for_loop_start:
+	ecall # print a0
+	addi a0, a0, -1 # i -= 1
+	bgt a0, zero, _for_loop_start # if t0 > 0 then target
 
 _exit:
 	li a0, 0            # exit code

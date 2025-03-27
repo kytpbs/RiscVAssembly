d75304224f8095daf3296f5f508eb9b718f30045
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-03-27 01:50:15 +0100

    use whisper to run app
    unfortunatly whisper doesn't use the same syscall table as RARS which
    means to stay the same with the code that the professor writes and is
    easier to read, i will run the app in RARS

--- run.ps1
+++ run.ps1
@@ -3,4 +3,4 @@ $ENV:path += ";C:\llvm\bin" # Add LLVM to PATH, couldn't get PATH to work for so
 
 clang --target=riscv64 -march=rv64gc -mabi=lp64d -mno-relax app.s -c -o build/app.o
 ld.lld build/app.o -o build/app.x
-wsl qemu-riscv64 ./build/app.x
+wsl whisper --newlib --log --stdin /dev/stdin --stdout /dev/stdout --stderr /dev/stderr ./build/app.x

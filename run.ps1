50c3f1c0443182b96e10a36785492c61ec9a5b4f
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-03-28 10:48:41 +0100

    switch to 32bit on clang

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 $ENV:path += ";C:\llvm\bin" # Add LLVM to PATH, couldn't get PATH to work for some reason...
 
-clang --target=riscv64 -march=rv64gc -mabi=lp64d -mno-relax app.s -c -o build/app.o
+clang --target=riscv32 -march=rv32gc -mabi=ilp32d -mno-relax app.s -c -o build/app.o
 ld.lld build/app.o -o build/app.x
 wsl whisper --newlib --log --stdin /dev/stdin --stdout /dev/stdout --stderr /dev/stderr ./build/app.x
